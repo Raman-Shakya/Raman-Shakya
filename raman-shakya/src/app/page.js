99afd93667f177ad0495f401f039441563337824
Raman Shakya <raman.gwarko15@gmail.com>
2024-06-22 12:39:59 +0545

    particlejs background

--- raman-shakya/src/app/page.js
+++ raman-shakya/src/app/page.js
@@ -2,12 +2,14 @@ import AboutMe from "./components/about_me/about_me";
 import Copyright from "./components/copyright";
 import Landing from "./components/landing";
 import NavBar from "./components/navbar";
+import Particle from "./components/particles/particles";
 import ProjectWrapper from "./components/projects/project-wrapper";
 import TechStack from "./components/tech-stack";
 
 export default function Home() {
   return (
     <main>
+      <Particle />
       <NavBar />
       <Landing />
       {/* <AboutMe /> */}
